df34032c55872ec27e9ecd9788351f5e34227ff0
meghalagarwal-konvergeai <meghal.agarwal@konverge.ai>
2022-04-25 19:09:27 +0530

    Python Practice

---
+++ Python/Functions.py
@@ -0,0 +1,10 @@
+# Complete the function by making sure it returns 42. .
+def return_42():
+    return 42
+
+# Create a function below, called my_function, that takes two arguments and returns the result of its two arguments multiplied together.
+def my_function(x, y):
+    return (x * y)
+
+print(return_42())
+print(my_function(2,2))
